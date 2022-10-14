d67eaae4d7148d3a5b62b5f55d06953cdca404ed
SpiReCZ <SpiReCZ@users.noreply.github.com>
2022-10-14 20:15:35 +0200

    obtain free ports

--- src/main/kotlin/cz/dwn/downloader/config/TorConfig.kt
+++ src/main/kotlin/cz/dwn/downloader/config/TorConfig.kt
@@ -1,23 +1,28 @@
 package cz.dwn.downloader.config
 
+import cz.dwn.downloader.tor.obtainFreePorts
+import org.berndpruenster.netlayer.tor.TorCtlException
 import org.berndpruenster.netlayer.tor.Torrc
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import java.io.IOException
 import java.util.*
 
+
 @Configuration
-class TorConfig {
+class TorConfig @Throws(TorCtlException::class) constructor() {
 
     @Bean
     @Throws(IOException::class)
     fun torrc(): Torrc {
-        val torrcEntries: LinkedHashMap<String, String> = LinkedHashMap<String, String>()
-        // It is required to set post statically
-        torrcEntries["ControlPort"] = "44443"
-        torrcEntries["SOCKSPort"] = "44444"
+        val ports = obtainFreePorts()
+        val overrides = LinkedHashMap<String, String>()
+        // It is required to set ports manually
+        overrides["ControlPort"] = ports.first.toString()
+        overrides["SOCKSPort"] = ports.second.toString()
         // We cannot run as Daemon
-        torrcEntries["RunAsDaemon"] = "0"
-        return Torrc(torrcEntries)
+        overrides["RunAsDaemon"] = "0"
+        return Torrc(overrides)
     }
+
 }
---
+++ src/main/kotlin/cz/dwn/downloader/tor/TorUtil.kt
@@ -0,0 +1,52 @@
+package cz.dwn.downloader.tor
+
+import org.berndpruenster.netlayer.tor.TorCtlException
+import java.net.InetAddress
+import java.net.InetSocketAddress
+import java.net.ServerSocket
+import java.util.concurrent.atomic.AtomicInteger
+
+
+fun obtainFreePorts(): Pair<Int, Int> {
+    val initialPortValue = 0
+    val port1 = AtomicInteger(initialPortValue)
+    val port2 = AtomicInteger(initialPortValue)
+
+    fun assignPortIfAvailable(target: AtomicInteger, value: Int): Boolean {
+        if (isTcpPortAvailable(value)) {
+            target.set(value)
+            return true
+        }
+        return false
+    }
+
+    // attempt to obtain 2 ports from client port range
+    for (int in 49152..65535) {
+        if (port1.get() == initialPortValue) {
+            assignPortIfAvailable(port1, int)
+        } else {
+            assignPortIfAvailable(port2, int + 1)
+            break
+        }
+    }
+
+    if (port1.get() == initialPortValue || port2.get() == initialPortValue) {
+        throw TorCtlException("Unable to obtain Tor Control port or Socks port")
+    }
+
+    return Pair(port1.get(), port2.get())
+}
+
+fun isTcpPortAvailable(port: Int): Boolean {
+    try {
+        ServerSocket().use { serverSocket ->
+            // setReuseAddress(false) is required only on macOS,
+            // otherwise the code will not work correctly on that platform
+            serverSocket.reuseAddress = false
+            serverSocket.bind(InetSocketAddress(InetAddress.getByName("localhost"), port), 1)
+            return true
+        }
+    } catch (ex: Exception) {
+        return false
+    }
+}

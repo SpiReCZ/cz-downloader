54b765287ba57e5b0dfbea6b301a038441765f77
SpiReCZ <SpiReCZ@users.noreply.github.com>
2022-10-15 23:34:37 +0200

    update to TorService

--- src/main/kotlin/cz/dwn/downloader/service/TorService.kt
+++ src/main/kotlin/cz/dwn/downloader/service/TorService.kt
@@ -1,12 +1,17 @@
 package cz.dwn.downloader.service
 
+import kotlinx.coroutines.*
+import org.apache.http.client.ClientProtocolException
 import org.apache.http.client.config.RequestConfig
+import org.apache.http.client.methods.CloseableHttpResponse
 import org.apache.http.client.methods.HttpGet
+import org.apache.http.client.methods.HttpRequestBase
 import org.apache.http.client.utils.URIBuilder
 import org.apache.http.impl.client.CloseableHttpClient
 import org.apache.http.util.EntityUtils
 import org.berndpruenster.netlayer.tor.Tor
 import org.berndpruenster.netlayer.tor.TorController
+import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Qualifier
 import org.springframework.boot.context.event.ApplicationReadyEvent
 import org.springframework.context.event.EventListener
@@ -17,11 +22,13 @@ import java.lang.reflect.Field
 import java.net.URI
 import java.nio.charset.StandardCharsets
 import java.time.Duration
+import kotlin.jvm.Throws
 
 
 @Service
-class TorService(private val tor: Tor, @Qualifier("torHttpClient") private val torHttpClient: CloseableHttpClient) {
+class TorService(tor: Tor, @Qualifier("torHttpClient") private val torHttpClient: CloseableHttpClient) {
 
+    private val log = LoggerFactory.getLogger("tor-service")
     private val torController: TorController
 
     init {
@@ -32,15 +39,12 @@ class TorService(private val tor: Tor, @Qualifier("torHttpClient") private val t
 
     @EventListener(ApplicationReadyEvent::class)
     fun doSomethingAfterStartup() {
-        test()
+        runBlocking {
+            test()
+        }
     }
 
-    fun Tor.reload() {
-        torController.setConf("DisableNetwork", "1")
-        torController.setConf("DisableNetwork", "0")
-    }
-
-    fun test() {
+    suspend fun test() {
         val url: URI = URIBuilder()
             .setScheme("https")
             .setHost("api.myip.com")
@@ -59,23 +63,45 @@ class TorService(private val tor: Tor, @Qualifier("torHttpClient") private val t
             val request = HttpGet(url)
             request.config = requestConfig
 
-            val body = Flux.interval(Duration.ofSeconds(10))
-                .flatMap { _: Long? ->
-                    try {
-                        torHttpClient.execute(request).use { response ->
-                            val entity: String = EntityUtils.toString(response!!.entity, StandardCharsets.UTF_8)
-                            return@flatMap Flux.just(entity)
-                        }
-                    } catch (e: IOException) {
-                        print("Exception while polling for ${url}: ${e.message}")
-                        return@flatMap Flux.empty<String>()
-                    }
-                }
-                .blockFirst(Duration.ofMinutes(3))
+            makeRequest(request).use {
+                print(EntityUtils.toString(it.entity, StandardCharsets.UTF_8))
+            }
 
-            print(body)
-            tor.reload()
-            Thread.sleep(2000)
+            reloadNetwork()
         }
     }
+
+    @Throws(IOException::class, ClientProtocolException::class)
+    suspend fun makeRequest(request: HttpRequestBase): CloseableHttpResponse {
+        val response = Flux.interval(Duration.ofSeconds(10))
+            .flatMap { _: Long? ->
+                try {
+                    return@flatMap Flux.just(torHttpClient.execute(request))
+                } catch (e: IOException) {
+                    print("Exception while polling for ${request.uri}: ${e.message}")
+                    return@flatMap Flux.empty<CloseableHttpResponse>()
+                }
+            }
+            .blockFirst(Duration.ofMinutes(3))
+        return response!!
+    }
+
+    suspend fun reloadNetwork() {
+        disableNetwork()
+        delay(1000)
+        enableNetwork()
+    }
+
+    suspend fun disableNetwork() {
+        torController.setConf(DISABLE_NETWORK_KEY, "1")
+    }
+
+    suspend fun enableNetwork() {
+        torController.setConf(DISABLE_NETWORK_KEY, "0")
+    }
+
+    companion object {
+        private const val DISABLE_NETWORK_KEY = "DisableNetwork"
+    }
+
 }
